fdd6fac90a61c9b71c182daf445f6e47dad6bc78
Vladlena <107118802+Vladlena2@users.noreply.github.com>
2023-11-28 18:44:01 +0300

    Для подключения к бд

---
+++ DatabaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+
+public class DatabaseHelper
+{
+    private readonly string connectionString;
+
+    // Конструктор класса, принимающий строку подключения к базе данных
+    public DatabaseHelper()
+    {
+        this.connectionString = "Host=localhost;Port=5432;Database=Coffeehouse;Username=postgres;Password=1234";
+    }
+
+    // Метод для выполнения SQL-запроса и обработки результатов
+    public void ExecuteQuery(string query, Action<NpgsqlDataReader> processRow)
+    {
+        // Используем using для гарантированного закрытия соединения даже в случае исключения
+        using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+        {
+            // Открываем соединение с базой данных
+            conn.Open();
+
+            // Создаем команду для выполнения SQL-запроса
+            using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+            {
+                // Используем using для автоматического закрытия читателя данных
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    // Читаем результаты запроса построчно
+                    while (reader.Read())
+                    {
+                        // Вызываем переданный делегат для обработки каждой строки результата
+                        processRow?.Invoke(reader);
+                    }
+                }
+            }
+        }
+    }
+}
