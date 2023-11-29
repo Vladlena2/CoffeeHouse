8eb749c47d2b4e6bfd2545b4671e46974417b035
Vladlena <107118802+Vladlena2@users.noreply.github.com>
2023-11-29 22:19:38 +0300

    Update DatabaseHelper.cs

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -1,40 +1,37 @@
-﻿using Npgsql;
+using Npgsql;
 using System;
 using System.Data;
 
 public class DatabaseHelper
 {
-    private readonly string connectionString;
+    private static readonly string connectionString = "Host=localhost;Port=5432;Database=Coffeehouse;Username=postgres;Password=1234";
+    NpgsqlConnection conn = new NpgsqlConnection(connectionString);
 
-    // Конструктор класса, принимающий строку подключения к базе данных
-    public DatabaseHelper()
+    public void openConnection()
     {
-        this.connectionString = "Host=localhost;Port=5432;Database=Coffeehouse;Username=postgres;Password=1234";
+        //Если связь закрыта 
+        if (conn.State == System.Data.ConnectionState.Closed) 
+        {
+            conn.Open();
+        }
+        
+    }
+
+    public void closeConnection()
+    {
+        //Если связь закрыта 
+        if (conn.State == System.Data.ConnectionState.Open) { }
+        conn.Open();
     }
 
     // Метод для выполнения SQL-запроса и обработки результатов
     public void ExecuteQuery(string query, Action<NpgsqlDataReader> processRow)
     {
-        // Используем using для гарантированного закрытия соединения даже в случае исключения
-        using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
-        {
-            // Открываем соединение с базой данных
-            conn.Open();
+        
+    }
 
-            // Создаем команду для выполнения SQL-запроса
-            using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
-            {
-                // Используем using для автоматического закрытия читателя данных
-                using (NpgsqlDataReader reader = cmd.ExecuteReader())
-                {
-                    // Читаем результаты запроса построчно
-                    while (reader.Read())
-                    {
-                        // Вызываем переданный делегат для обработки каждой строки результата
-                        processRow?.Invoke(reader);
-                    }
-                }
-            }
-        }
+    public NpgsqlConnection getConnection()
+    {
+        return conn;
     }
 }
