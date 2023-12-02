4c968d3dd6c7e55c4ef76baed4a97a639aa58095
Vladlena <107118802+Vladlena2@users.noreply.github.com>
2023-12-02 13:00:34 +0300

    Update Form10.Designer.cs

--- Form10.Designer.cs
+++ Form10.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Soft_Coffee
+namespace Soft_Coffee
 {
     partial class Form10
     {
@@ -40,23 +40,21 @@
             label1 = new Label();
             button1 = new Button();
             dataGridView2 = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
             Column3 = new DataGridViewComboBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewCheckBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
             label3 = new Label();
             panel2 = new Panel();
-            label6 = new Label();
             button3 = new Button();
             dataGridView1 = new DataGridView();
+            button2 = new Button();
+            label4 = new Label();
             dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
             dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
             dataGridViewComboBoxColumn1 = new DataGridViewComboBoxColumn();
             dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
-            label5 = new Label();
-            button2 = new Button();
-            label4 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             panel2.SuspendLayout();
@@ -69,18 +67,17 @@
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Location = new Point(-1, 0);
-            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1412, 59);
+            panel1.Size = new Size(1236, 44);
             panel1.TabIndex = 1;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Baskerville Old Face", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(714, 9);
+            label2.Location = new Point(625, 7);
             label2.Name = "label2";
-            label2.Size = new Size(95, 34);
+            label2.Size = new Size(69, 27);
             label2.TabIndex = 1;
             label2.Text = "Заказ";
             // 
@@ -88,22 +85,20 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Baskerville Old Face", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(13, 9);
+            label1.Location = new Point(11, 7);
             label1.Name = "label1";
-            label1.Size = new Size(91, 34);
+            label1.Size = new Size(68, 27);
             label1.TabIndex = 0;
             label1.Text = "Меню";
-            label1.Click += label1_Click;
             // 
             // button1
             // 
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Baskerville Old Face", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.FromArgb(238, 219, 188);
-            button1.Location = new Point(13, 284);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(11, 213);
             button1.Name = "button1";
-            button1.Size = new Size(327, 45);
+            button1.Size = new Size(286, 34);
             button1.TabIndex = 1;
             button1.Text = "Добавить в заказ";
             button1.UseVisualStyleBackColor = true;
@@ -129,7 +124,6 @@
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
             dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
             dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = SystemColors.Window;
             dataGridViewCellStyle3.Font = new Font("Baskerville Old Face", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
@@ -139,23 +133,16 @@
             dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
             dataGridView2.DefaultCellStyle = dataGridViewCellStyle3;
             dataGridView2.GridColor = Color.FromArgb(152, 117, 88);
-            dataGridView2.Location = new Point(714, -7);
+            dataGridView2.Location = new Point(625, -5);
+            dataGridView2.Margin = new Padding(3, 2, 3, 2);
             dataGridView2.Name = "dataGridView2";
             dataGridView2.ReadOnly = true;
             dataGridView2.RowHeadersWidth = 51;
             dataGridView2.RowTemplate.Height = 29;
             dataGridView2.ScrollBars = ScrollBars.None;
             dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView2.Size = new Size(677, 288);
+            dataGridView2.Size = new Size(592, 216);
             dataGridView2.TabIndex = 2;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "Номер напитка";
-            Column1.MinimumWidth = 6;
-            Column1.Name = "Column1";
-            Column1.ReadOnly = true;
-            Column1.SortMode = DataGridViewColumnSortMode.NotSortable;
             // 
             // Column2
             // 
@@ -188,54 +175,45 @@
             Column5.Name = "Column5";
             Column5.ReadOnly = true;
             // 
+            // Column1
+            // 
+            Column1.Name = "Column1";
+            // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Baskerville Old Face", 18F, FontStyle.Regular, GraphicsUnit.Point);
             label3.ForeColor = Color.FromArgb(238, 219, 188);
-            label3.Location = new Point(714, 284);
+            label3.Location = new Point(625, 213);
             label3.Name = "label3";
-            label3.Size = new Size(172, 34);
+            label3.Size = new Size(120, 27);
             label3.TabIndex = 3;
             label3.Text = "Общая цена";
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(152, 117, 88);
-            panel2.Controls.Add(label6);
             panel2.Controls.Add(button3);
             panel2.Controls.Add(dataGridView1);
-            panel2.Controls.Add(label5);
             panel2.Controls.Add(button2);
             panel2.Controls.Add(label4);
             panel2.Controls.Add(label3);
             panel2.Controls.Add(dataGridView2);
             panel2.Controls.Add(button1);
-            panel2.Location = new Point(-1, 53);
-            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Location = new Point(-1, 40);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1515, 876);
+            panel2.Size = new Size(1326, 657);
             panel2.TabIndex = 2;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Baskerville Old Face", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.ForeColor = Color.FromArgb(238, 219, 188);
-            label6.Location = new Point(721, 322);
-            label6.Name = "label6";
-            label6.Size = new Size(58, 23);
-            label6.TabIndex = 9;
-            label6.Text = "label6";
             // 
             // button3
             // 
             button3.FlatStyle = FlatStyle.Flat;
             button3.Font = new Font("Baskerville Old Face", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
             button3.ForeColor = Color.FromArgb(238, 219, 188);
-            button3.Location = new Point(13, 336);
+            button3.Location = new Point(11, 252);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(327, 45);
+            button3.Size = new Size(286, 34);
             button3.TabIndex = 8;
             button3.Text = "Скидочная карта";
             button3.UseVisualStyleBackColor = true;
@@ -261,7 +239,6 @@
             dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
             dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewComboBoxColumn1, dataGridViewTextBoxColumn3 });
             dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle6.BackColor = SystemColors.Window;
             dataGridViewCellStyle6.Font = new Font("Baskerville Old Face", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
@@ -271,68 +248,28 @@
             dataGridViewCellStyle6.WrapMode = DataGridViewTriState.False;
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
             dataGridView1.GridColor = Color.FromArgb(152, 117, 88);
-            dataGridView1.Location = new Point(13, 0);
+            dataGridView1.Location = new Point(11, 0);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.ScrollBars = ScrollBars.None;
+            dataGridView1.ScrollBars = ScrollBars.Vertical;
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.Size = new Size(677, 281);
+            dataGridView1.Size = new Size(592, 211);
             dataGridView1.TabIndex = 7;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            dataGridViewTextBoxColumn1.HeaderText = "Номер напитка";
-            dataGridViewTextBoxColumn1.MinimumWidth = 6;
-            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            dataGridViewTextBoxColumn1.ReadOnly = true;
-            dataGridViewTextBoxColumn1.SortMode = DataGridViewColumnSortMode.NotSortable;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            dataGridViewTextBoxColumn2.HeaderText = "Название";
-            dataGridViewTextBoxColumn2.MinimumWidth = 6;
-            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            dataGridViewTextBoxColumn2.ReadOnly = true;
-            dataGridViewTextBoxColumn2.SortMode = DataGridViewColumnSortMode.NotSortable;
-            // 
-            // dataGridViewComboBoxColumn1
-            // 
-            dataGridViewComboBoxColumn1.HeaderText = "Объем";
-            dataGridViewComboBoxColumn1.MinimumWidth = 6;
-            dataGridViewComboBoxColumn1.Name = "dataGridViewComboBoxColumn1";
-            dataGridViewComboBoxColumn1.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            dataGridViewTextBoxColumn3.HeaderText = "Цена";
-            dataGridViewTextBoxColumn3.MinimumWidth = 6;
-            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            dataGridViewTextBoxColumn3.ReadOnly = true;
-            dataGridViewTextBoxColumn3.Resizable = DataGridViewTriState.True;
-            dataGridViewTextBoxColumn3.SortMode = DataGridViewColumnSortMode.NotSortable;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Baskerville Old Face", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.ForeColor = Color.FromArgb(238, 219, 188);
-            label5.Location = new Point(765, 322);
-            label5.Name = "label5";
-            label5.Size = new Size(18, 23);
-            label5.TabIndex = 6;
-            label5.Text = "₽";
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // button2
             // 
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Baskerville Old Face", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
             button2.ForeColor = Color.FromArgb(238, 219, 188);
-            button2.Location = new Point(1064, 284);
+            button2.Location = new Point(931, 213);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(327, 45);
+            button2.Size = new Size(286, 34);
             button2.TabIndex = 5;
             button2.Text = "Перейти к оплате";
             button2.UseVisualStyleBackColor = true;
@@ -343,22 +280,37 @@
             label4.AutoSize = true;
             label4.Font = new Font("Baskerville Old Face", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label4.ForeColor = Color.FromArgb(238, 219, 188);
-            label4.Location = new Point(721, 322);
+            label4.Location = new Point(631, 242);
             label4.Name = "label4";
-            label4.Size = new Size(58, 23);
+            label4.Size = new Size(46, 18);
             label4.TabIndex = 4;
             label4.Text = "label4";
             label4.Visible = false;
             // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewComboBoxColumn1
+            // 
+            dataGridViewComboBoxColumn1.Name = "dataGridViewComboBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
             // Form10
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1402, 437);
+            ClientSize = new Size(1227, 328);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form10";
             Text = "Menu";
             Load += Form10_Load;
@@ -377,6 +329,7 @@
         private Label label2;
         private Button button1;
         private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewComboBoxColumn Column3;
@@ -386,13 +339,10 @@
         private Panel panel2;
         private Label label4;
         private Button button2;
-        private Label label5;
-        private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private DataGridViewComboBoxColumn dataGridViewComboBoxColumn1;
         private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private Button button3;
-        private Label label6;
     }
 }
