5a18d11a31b110f1a05ca489ac51098dd29f0648
NoFirstReal <152307039+NoFirstReal@users.noreply.github.com>
2023-12-02 12:41:41 +0300

    Добавил авторизацию с подвязкой к БД

--- Form12.cs
+++ Form12.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Npgsql;
 
 namespace Soft_Coffee
 {
     public partial class Form12 : Form
     {
+        DatabaseHelper database = new DatabaseHelper();
+
         public Form12()
         {
             InitializeComponent();
@@ -30,13 +33,33 @@ namespace Soft_Coffee
             {
                 label4.Visible = true;
             }
-            else
+            var loginUser = textBox1.Text;
+            var passUser = textBox2.Text;
+
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string queryString = "SELECT id, name, password FROM users WHERE name = @loginUser AND password = @passUser";
+
+            NpgsqlCommand command = new NpgsqlCommand(queryString, database.getConnection());
+            command.Parameters.AddWithValue("@loginUser", loginUser);
+            command.Parameters.AddWithValue("@passUser", passUser);
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count == 1)
             {
                 label4.Visible = false;
                 MessageBox.Show("Вы успешно вошли", "Успешно!", MessageBoxButtons.OK);
                 Form1 newForm = new Form1();
                 this.Hide();
                 newForm.ShowDialog();
+            }
+                
+            else
+            {
+                MessageBox.Show("Такого аккаунта не существует", "Аккаунта не существует!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
         }
